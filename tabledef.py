2c0ea710f322b32448be7b1e9e9e027b992870a9
remmi11 <wtgeographer@gmail.com>
2017-12-12 15:36:42 -0600

    awesome updates

--- tabledef.py
+++ tabledef.py
@@ -17,7 +17,7 @@ Base = declarative_base()
 ########################################################################
 class User(Base):
     """"""
-    __tablename__ = "users"
+    __tablename__ = "user"
  
     id = Column(Integer, primary_key=True)
     username = Column(String)
